230fb4267131f38bea6df8d82fd4a41f2e14dd0d
amon <amon@nandynarwhals.org>
2013-10-28 22:50:06 +0800

    Image tests (OpenCV) added

--- SOA Projects/Euclid/Experiments/test.py
+++ SOA Projects/Euclid/Experiments/test.py
@@ -1,3 +1,11 @@
-if __name__ == "__main__":
-	inp = raw_input("What do you wish to say?")
-	print "Hi, " + inp
+# GrayScale Image Convertor
+# http://extr3metech.wordpress.com
+ 
+import cv2
+image = cv2.imread('im1.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('gray_image.png',gray_image)
+cv2.imshow('color_image',image)
+cv2.imshow('gray_image',gray_image) 
+cv2.waitKey(0)                 # Waits forever for user to press any key
+cv2.destroyAllWindows()        # Closes displayed windows
